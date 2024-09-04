d3a1847fa8c5c215dd79659e0b03adf8df94b326
bhanu <bhanu@BHANU>
2024-09-04 12:19:48 +0530

    First commit to master branch

---
+++ GitDemo/src/com/gitDemo/Test.java
@@ -0,0 +1,20 @@
+package com.gitDemo;
+
+public class Test {
+
+	public void TestA1()
+	{
+		int a = 50;
+		int b = 60;
+		int c = a+b;
+		if(c == 110)
+		{
+			System.out.println("C value = "+ c);
+		}
+		else
+		{
+			System.out.println("C Value is not Equal");
+		}
+			
+	}
+}
